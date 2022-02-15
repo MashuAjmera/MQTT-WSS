8d76d40451f9a8288f936fbc216610703b675cc0
Mashu Ajmera <32427540+MashuAjmera@users.noreply.github.com>
2022-02-15 21:15:39 +0530

    Update pub.py

--- pub.py
+++ pub.py
@@ -1,35 +1,14 @@
-# python 3.6
-
-import random
+import random  
 import time
-
 from paho.mqtt import client as mqtt_client
 
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT Broker!")
+    else:
+        print("Failed to connect, return code %d\n", rc)
 
-broker = 'localhost'
-port = 9001
-topic = "python/wss"
-# generate client ID with pub prefix randomly
-client_id = f'python-mqtt-{random.randint(0, 1000)}'
-# username = 'emqx'
-# password = 'public'
-
-def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
-
-    client = mqtt_client.Client(client_id,transport='websockets')
-    # client.username_pw_set(username, password)
-    client.tls_set('ca.crt',tls_version=2)
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
-
-
-def publish(client):
+def publish(client,topic):
     msg_count = 0
     while True:
         time.sleep(1)
@@ -42,13 +21,19 @@ def publish(client):
         else:
             print(f"Failed to send message to topic {topic}")
         msg_count += 1
-
-
-def run():
-    client = connect_mqtt()
-    client.loop_start()
-    publish(client)
-
-
+        
 if __name__ == '__main__':
-    run()
+    broker = 'localhost'
+    port = 9001
+    topic = "python/wss" 
+    # generate client ID with pub prefix randomly
+    client_id = f'python-mqtt-{random.randint(0, 1000)}'
+    # username = 'emqx'
+    # password = 'public'
+    client = mqtt_client.Client(client_id,transport='websockets')
+    # client.username_pw_set(username, password)
+    client.tls_set('ca.crt',tls_version=2)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    client.loop_start()
+    publish(client,topic)

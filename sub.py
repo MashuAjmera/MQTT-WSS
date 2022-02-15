3bceaa8f7fcdd53f5c5160b7af840d999a808fbe
Mashu Ajmera <32427540+MashuAjmera@users.noreply.github.com>
2022-02-15 21:04:47 +0530

    Update sub.py

--- sub.py
+++ sub.py
@@ -1,47 +1,27 @@
-# python3.6
-
 import random
-
 from paho.mqtt import client as mqtt_client
 
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT Broker!")
+    else:
+        print("Failed to connect, return code %d\n", rc)
 
-broker = 'localhost'
-port = 9001
-topic = "python/wss"
-# generate client ID with pub prefix randomly
-client_id = f'python-mqtt-{random.randint(0, 100)}'
-# username = 'emqx'
-# password = 'public'
+def on_message(client, userdata, msg):
+    print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
 
-
-def connect_mqtt() -> mqtt_client:
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
-
+if __name__ == '__main__':
+    broker = 'localhost'
+    port = 9001
+    topic = "python/wss"
+    client_id = f'python-mqtt-{random.randint(0, 100)}'
+    # username = 'emqx'
+    # password = 'public'
     client = mqtt_client.Client(client_id,transport='websockets')
     # client.username_pw_set(username, password)
     client.tls_set('ca.crt',tls_version=2)
     client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
-
-
-def subscribe(client: mqtt_client):
-    def on_message(client, userdata, msg):
-        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-
-    client.subscribe(topic)
     client.on_message = on_message
-
-
-def run():
-    client = connect_mqtt()
-    subscribe(client)
+    client.connect(broker, port)
+    client.subscribe(topic)
     client.loop_forever()
-
-
-if __name__ == '__main__':
-    run()
